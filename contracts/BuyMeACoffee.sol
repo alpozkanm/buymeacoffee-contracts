fd927a653a436f1feba40a79fb578699e1025b9b
Alp Ozkan <alpozkan@Alps-MacBook-Air.local>
2023-08-15 17:27:33 +0300

    untracked files updated

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -1,56 +1,46 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.4;
 
 // Switch this to your own contract address once deployed, for bookkeeping!
 // Example Contract Address on Goerli: 0xDBa03676a2fBb6711CB652beF5B7416A53c1421D
 
 contract BuyMeACoffee {
     // Event to emit when a Memo is created.
-    event NewMemo(
+    event newMemo(
         address indexed from,
         uint256 timestamp,
         string name,
         string message
     );
-    
-    // Memo struct.
-    struct Memo {
+
+    // Memo struct. what a memo is
+    struct Memo{
         address from;
         uint256 timestamp;
         string name;
         string message;
     }
-    
-    // Address of contract deployer. Marked payable so that
-    // we can withdraw to this address later.
-    address payable owner;
 
-    // List of all memos received from coffee purchases.
+    // list of all memos received from friends
     Memo[] memos;
 
-    constructor() {
-        // Store the address of the deployer as a payable address.
-        // When we withdraw funds, we'll withdraw here.
+    //address of contract deployer.
+    address payable owner;   
+
+    //Deploy logic
+    constructor(){
         owner = payable(msg.sender);
+
+
     }
 
-    /**
-     * @dev fetches all stored memos
-     */
-    function getMemos() public view returns (Memo[] memory) {
-        return memos;
-    }
+    // to buy a coddee to contract owner
+    // @param _name name of the coffee buyer
+    // @param _message a nice message from the coffee buyer
+    function buyCoffee(string memory _name, string memory _message) public payable{
+        require(msg.value>0, "Can't buy coffe with 0 eth");
 
-    /**
-     * @dev buy a coffee for owner (sends an ETH tip and leaves a memo)
-     * @param _name name of the coffee purchaser
-     * @param _message a nice message from the purchaser
-     */
-    function buyCoffee(string memory _name, string memory _message) public payable {
-        // Must accept more than 0 ETH for a coffee.
-        require(msg.value > 0, "can't buy coffee for free!");
-
-        // Add the memo to storage!
+        // add to memo to storage
         memos.push(Memo(
             msg.sender,
             block.timestamp,
@@ -58,19 +48,23 @@ contract BuyMeACoffee {
             _message
         ));
 
-        // Emit a NewMemo event with details about the memo.
-        emit NewMemo(
+        // emit a log event when new memo created
+        emit newMemo(
             msg.sender,
             block.timestamp,
             _name,
-            _message
-        );
+            _message);
     }
 
-    /**
-     * @dev send the entire balance stored in this contract to the owner
-     */
-    function withdrawTips() public {
-        require(owner.send(address(this).balance));
+    // send entire balance stored in this contract to owner
+    function withdrawTips() public{
+        require(owner.send(address(this.balance));
+
     }
+
+    // retrieve all the memos stored in blockchain
+    function getMemos() public {
+
+    }
+
 }
